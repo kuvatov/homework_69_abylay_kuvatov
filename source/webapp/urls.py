8df5335b89776f7f9fe65c4b9701dec665d6c43a
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-14 11:37:50 +0600

    Добавил представление для второго задания, также добавил шаблон index.html

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from webapp.views import get_token_view, calculate
+from webapp.views.task1 import get_token_view, calculate
+from webapp.views.task2 import IndexView
 
 urlpatterns = [
     path('get_token/', get_token_view, name='get_token'),
-    path('<action>/', calculate, name='calculate')
+    path('<action>/', calculate, name='calculate'),
+    path('', IndexView.as_view(), name='index')
 ]
--- source/webapp/views/task1.py
+++ source/webapp/views/task1.py
@@ -8,7 +8,6 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 def get_token_view(request, *args, **kwargs):
     if request.method == 'GET':
         return HttpResponse()
-
     return HttpResponseNotAllowed('Only GET request are allowed')
 
 
